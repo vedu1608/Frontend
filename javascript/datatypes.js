930998767a6fe93fa1e9e0d72f2ad872eef9c6ee
vedu1608 <vedikadate2004@gmail.com>
2026-04-15 18:44:23 +0530

    add datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all the code as a newer version of JavaScript
+//alert("hello world") we use not js not browser //shows a pop up box with the message "hello world"
+
+
+//Data types in JavaScript
+//1. String
+let name = "vedika"
+let age = 20    //number
+let  isloggedIn = true //boolean
+let  nullValue = null //null
+let undefinedValue; //undefined
+let symbolValue = Symbol("unique") //symbol
+let bigIntValue = 1234567890123456789012345678901234567890n //bigint
+let objectValue = { name: "vedika", age: 20 } //object
+let arrayValue = [1, 2, 3, 4, 5] //array
+
+
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof isloggedIn) //boolean
+console.log(typeof nullValue) //object
+console.log(typeof undefinedValue) //undefined
+
+console.log(typeof symbolValue) //symbol
+console.log(typeof bigIntValue) //bigint
+console.log(typeof objectValue) //object
+console.log(typeof arrayValue) //object (arrays are a type of object in JavaScript)
+
