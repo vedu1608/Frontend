c0ac5367089a65927745ae762d4cd7b1eb262258
vedu1608 <vedikadate2004@gmail.com>
2026-04-15 18:51:32 +0530

    new file update

---
+++ javascript/conversionoperation.js
@@ -0,0 +1,9 @@
+let number = "42"
+
+console.log("String value:", number) // String value: 42
+console.log("Type before conversion:", typeof number) // Type before conversion: string
+
+// Converting string to number
+let convertedNumber = Number(number)
+console.log("Converted number:", convertedNumber) // Converted number: 42
+console.log("Type after conversion:", typeof convertedNumber) // Type after conversion: number
