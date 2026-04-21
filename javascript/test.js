b948dcfeedd47def8b059c7f9079ca97023dd5c6
vedu1608 <vedikadate2004@gmail.com>
2026-04-21 18:15:29 +0530

    just adding new in test.py

--- javascript/test.js
+++ javascript/test.js
@@ -1,5 +1,12 @@
+vedika = console // here we are assigning the console object to a variable named vedika bcz console is a global object in JavaScript that provides methods for logging and debugging. By assigning it to vedika, we can use vedika to access the same methods and properties as console. This allows us to log messages and perform other console-related tasks using the vedika variable instead of console. For example, we can use vedika.log("Hello, World!") to log a message to the console.
+
+
 console.log("vedika")
-console.log("vedika")
+vedika.log("vedika by using vedika variable rather than console") // vedika by using vedika variable rather than console
+
+print = console.log // here we are assigning the console.log method to a variable named print. This allows us to use print as a shorthand for console.log, making it easier and more concise to log messages to the console. For example, we can use print("Hello, World!") instead of console.log("Hello, World!") to achieve the same result.
+
+print("vedika by using print variable rather than console.log") // vedika by using print variable rather than console.log
 
 
 
