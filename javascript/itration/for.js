892335803b9bb680f94a6ad6c9ff3de105b23cae
vedu1608 <vedikadate2004@gmail.com>
2026-04-27 14:36:22 +0530

    changes done

--- javascript/itration/for.js
+++ javascript/itration/for.js
@@ -1,4 +1,17 @@
 for (let index = 0; index <10; index++) {
     const element = index;
+    if(element==6){
+        console.log("6 is hitting")
+    }
     console.log(element)
+}
+
+
+for (let index = 0; index < 5; index++) {
+   console.log(`outerloop value :${index}`);
+    for (let i = 0; i < 5; i++) {
+        console.log(`inner loop value:${i}`);
+        
+    }
+    
 }
