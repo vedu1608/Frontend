d608feafc34e9a205997acb83f12fc21e5d71dcf
vedu1608 <vedikadate2004@gmail.com>
2026-04-27 14:28:13 +0530

    new file

--- javascript/control_flow/if.js
+++ javascript/control_flow/if.js
@@ -7,3 +7,36 @@ if(isuserlog){
 else {
     console.log('not')
 }
+
+const math =false
+const science = false
+if(math && science){
+    console.log("enroll for both neet and jee")
+
+}
+if (math || science){
+    console.log("enroll for jee either science")
+
+}
+else{
+    console.log("not eligible")
+}
+
+
+// flasly value =false, 0, -0, 0n, "", null, undefined, NaN
+
+// truthy value=true, 1, -1, "text", "false", [], {}, function(){} "0"
+
+
+//Nullish Coalescing operator(??):Null undefined
+
+
+let val1;
+val1=2?? 10
+console.log(val1)
+
+// terniary operator
+//condition? true:false
+
+
+
---
+++ javascript/control_flow/switch.js
@@ -0,0 +1,19 @@
+const subject = "math";
+
+switch (subject) {
+  case "math":
+    console.log("Prepare for JEE");
+    break;
+
+  case "science":
+    console.log("Prepare for NEET");
+    break;
+
+  case "both":
+    console.log("Prepare for both JEE and NEET");
+    break;
+
+  default:
+    console.log("Not eligible");
+}
+
